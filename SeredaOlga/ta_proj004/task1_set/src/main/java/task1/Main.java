b5fef3b8e37e6895ba9bb7d84bd2ddbb7f1ac58c
hellvita <seredaolga03@gmail.com>
2023-11-06 10:39:23 +0200

    added Set task

---
+++ SeredaOlga/ta_proj004/task1_set/src/main/java/task1/Main.java
@@ -0,0 +1,40 @@
+package task1;
+
+import java.util.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Set<Integer> mySet1 = new HashSet<>();
+        mySet1.add(6);
+        mySet1.add(2);
+        mySet1.add(4);
+        mySet1.add(5);
+
+        Set<Integer> mySet2 = new HashSet<>();
+        mySet2.add(6);
+        mySet2.add(3);
+        mySet2.add(5);
+        mySet2.add(1);
+
+        Set<Integer> uSet;
+        MyMethods methods = new MyMethods();
+        
+
+        System.out.print("\n\nunion:\n");
+        uSet = methods.union(mySet1, mySet2);
+        for (Integer element : uSet){
+            System.out.print(element + "  ");
+        }
+
+        System.out.print("\n\nintersect:\n");
+        uSet = methods.intersect(mySet1, mySet2);
+        for (Integer element : uSet){
+            System.out.print(element + "  ");
+        }
+
+    }
+}
---
+++ SeredaOlga/ta_proj004/task1_set/src/main/java/task1/MyMethods.java
@@ -0,0 +1,23 @@
+package task1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MyMethods {
+    public <T> Set<T> union(Set<T> set1, Set<T> set2){
+        Set<T> set = new HashSet<>();
+        set.addAll(set1);
+        set.addAll(set2);
+        return set;
+    }
+
+    public <T> Set<T> intersect(Set<T> set1, Set<T> set2){
+        Set<T> set = new HashSet<>();
+        for (T element : set1){
+            if (set2.contains(element)){
+                set.add(element);
+            }
+        }
+        return set;
+    }
+}
